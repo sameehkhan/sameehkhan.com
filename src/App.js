fad5d264103111d40042fb4e3f4d36d9d40bce43
Sameeh <sameehkhan1@gmail.com>
2019-01-06 19:57:08 -0800

    Create page container

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ const linkedin = require('./images/linkedin.png');
 class App extends Component {
   render() {
     return (
-      <div className='splash-container'>
-        <img className='logo' src={logo} />
-        <div className='info'>
-          <div>Sameeh Khan</div>
-          <div>Full-Stack Developer</div>
-        </div>
-        <div className='links'>
-        <a href='https://github.com/sameehkhan'><img className='git' src={git}/></a>
-        <a href='https://www.linkedin.com/in/sameeh-khan-877657b4/'><img className='linkedin' src={linkedin}/></a>
+      <div className='page-container'>
+        <div className='splash-container'>
+          <img className='logo' src={logo} />
+          <div className='info'>
+            <div>Sameeh Khan</div>
+            <div>Full-Stack Developer</div>
+          </div>
+          <div className='links'>
+            <div><a href='https://github.com/sameehkhan'><img className='git' src={git} /></a></div>
+            <div><a href='https://www.linkedin.com/in/sameeh-khan-877657b4/'><img className='linkedin' src={linkedin} /></a></div>
+          </div>
         </div>
       </div>
     );
