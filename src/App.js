064ccb8e0f741369afcbeef1b194f2e17572f57b
Sameeh <sameehkhan1@gmail.com>
2019-01-06 20:54:11 -0800

    Add spinner to splash

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ const linkedin = require('./images/linkedin.png');
 
 
 
+
+
 class App extends Component {
   render() {
     return (
@@ -15,6 +17,39 @@ class App extends Component {
           <div className='info'>
             <div className='name'><div>Sameeh Khan</div></div>
             <div className='developer'><div>Full-Stack Developer</div></div>
+            <div className='developer'><div>Site in-progress</div></div>
+
+            <div className='spinner'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="64" height="64" fill="black">
+              <circle cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(45 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.125s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(90 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.25s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(135 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.375s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(180 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(225 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.625s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(270 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.75s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(315 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.875s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+              <circle transform="rotate(180 16 16)" cx="16" cy="3" r="0">
+                <animate attributeName="r" values="0;3;0;0" dur="1s" repeatCount="indefinite" begin="0.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+              </circle>
+            </svg>
+            </div>
+
           </div>
           <div className='links'>
             <div><a href='https://github.com/sameehkhan' className='link-tag'><img className='git' src={git} /></a></div>
