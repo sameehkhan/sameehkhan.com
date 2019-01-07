b2d7dc6a6ce99084a6c9cf4f885116c4d03d66da
Sameeh <sameehkhan1@gmail.com>
2019-01-06 19:35:09 -0800

    Import link from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 const logo = require('./images/sk.png');
 const git = require('./images/github.png');
